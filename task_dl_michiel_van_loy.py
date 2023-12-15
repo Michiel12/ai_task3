0726ffb4d180bd70acb33a2ac4dc205b836daa8d
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-12-15 15:06:46 +0100

    Update task_dl_michiel_van_loy.py

--- task_dl_michiel_van_loy.py
+++ task_dl_michiel_van_loy.py
@@ -50,39 +50,47 @@ plt.show()
 
 from keras.preprocessing.image import ImageDataGenerator
 
-# Define directories
-train_dir = 'google_images/training_set'
-test_dir = 'google_images/test_set'
+# Example usage:
+training_set, validation_set, test_set = generate_augmented_data()
 
-# Augmentation variables for training set
-train_val_datagen = ImageDataGenerator(validation_split=0.2,
-                                   rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   vertical_flip= True)
+def generate_augmented_data():
+  # Define directories
+  train_dir = 'google_images/training_set'
+  test_dir = 'google_images/test_set'
 
-# Augmentation variables for training set
-test_datagen = ImageDataGenerator(rescale = 1./255)
+  # Augmentation variables for training set
+  train_val_datagen = ImageDataGenerator(validation_split=0.2,
+                                    rescale = 1./255,
+                                    shear_range = 0.2,
+                                    zoom_range = 0.2,
+                                    vertical_flip= True)
 
-# Generate batches of augmented data for training set
-training_set = train_val_datagen.flow_from_directory(train_dir,
-                                                 subset='training',
-                                                 target_size = (128, 128),
-                                                 batch_size = 32,
-                                                 class_mode = 'categorical') 
+  # Augmentation variables for training set
+  test_datagen = ImageDataGenerator(rescale = 1./255)
 
-# Generate batches of augmented data for the validation set
-validation_set = train_val_datagen.flow_from_directory(train_dir,
-                                                 subset='validation',
-                                                 target_size = (128, 128),
-                                                 batch_size = 32,
-                                                 class_mode = 'categorical')
+  # Generate batches of augmented data for training set
+  training_set = train_val_datagen.flow_from_directory(train_dir,
+                                                  subset='training',
+                                                  target_size = (128, 128),
+                                                  batch_size = 32,
+                                                  class_mode = 'categorical') 
 
-# Generate batches of augmented data for the test set
-test_set = test_datagen.flow_from_directory(test_dir,
-                                            target_size = (128, 128),
-                                            batch_size = 32,
-                                            class_mode = 'categorical')
+  # Generate batches of augmented data for the validation set
+  validation_set = train_val_datagen.flow_from_directory(train_dir,
+                                                  subset='validation',
+                                                  target_size = (128, 128),
+                                                  batch_size = 32,
+                                                  class_mode = 'categorical')
+
+  # Generate batches of augmented data for the test set
+  test_set = test_datagen.flow_from_directory(test_dir,
+                                              target_size = (128, 128),
+                                              batch_size = 32,
+                                              class_mode = 'categorical')
+  
+  return training_set, validation_set, test_set
+
+
 
 
 
@@ -92,71 +100,103 @@ from tensorflow import keras
 from tensorflow.keras import optimizers
 from tensorflow.keras import layers
 
-NUM_CLASSES = 5
+# Example usage:
+model = create_model()
 
-# Create a sequential model with a list of layers
-model = tf.keras.Sequential([
-  layers.Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation="relu"),
-  layers.MaxPooling2D((2, 2)),
-  layers.Dropout(0.3),
-  layers.Conv2D(32, (3, 3), activation="relu"),
-  layers.MaxPooling2D((2, 2)),
-  layers.Dropout(0.3),
-  layers.Flatten(),
-  layers.Dense(128, activation="relu"),
-  layers.Dense(NUM_CLASSES, activation="softmax")
-])
+def create_model():
+  NUM_CLASSES = 5
 
-# Compile model
-model.compile(optimizer = optimizers.Adam(learning_rate=0.001), 
-              loss = 'categorical_crossentropy', 
-              metrics = ['accuracy'])
+  # Create a sequential model with a list of layers
+  model = tf.keras.Sequential([
+    layers.Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.3),
+    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.3),
+    layers.Flatten(),
+    layers.Dense(128, activation="relu"),
+    layers.Dense(NUM_CLASSES, activation="softmax")
+  ])
 
-print(model.summary())
+  # Compile model
+  model.compile(optimizer = optimizers.Adam(learning_rate=0.001), 
+                loss = 'categorical_crossentropy', 
+                metrics = ['accuracy'])
 
-
-
-history = model.fit(training_set,
-                validation_data = validation_set,
-                steps_per_epoch = 15,
-                # steps_per_epoch = 10,
-                epochs = 25
-                )
+  print(model.summary())
+  
+  return model
 
 
 
-# Check test accuracy
-test_loss, test_acc = model.evaluate(test_set)
-print('Test accuracy:', test_acc)
+
+
+
+# Example usage:
+trained_history = train_model(model, training_set, validation_set, epochs=25, steps_per_epoch=15)
+
+def train_model(model, training_set, validation_set, epochs=25, steps_per_epoch=15):
+  history = model.fit(training_set,
+                  validation_data = validation_set,
+                  steps_per_epoch = steps_per_epoch,
+                  epochs = epochs
+                  )
+  return history
+
+
+
+
+
+# Example usage:
+test_loss, test_acc = test_accuracy(model, test_set)
+
+def evaluate_test_accuracy(model, test_set):
+  # Check test accuracy
+  test_loss, test_acc = model.evaluate(test_set)
+  print('Test accuracy:', test_acc)
+  
+  return test_loss, test_acc
+
+
+
 
 
 
 import matplotlib.pyplot as plt
 
-# Create a figure and a grid of subplots with a single call
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
+# Example usage:
+loss_and_accuracy(history)
 
-# Plot the loss curves on the first subplot
-ax1.plot(history.history['loss'], label='training loss')
-ax1.plot(history.history['val_loss'], label='validation loss')
-ax1.set_title('Loss curves')
-ax1.set_xlabel('Epoch')
-ax1.set_ylabel('Loss')
-ax1.legend()
+def loss_and_accuracy(history):
+  # Create a figure and a grid of subplots with a single call
+  fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
 
-# Plot the accuracy curves on the second subplot
-ax2.plot(history.history['accuracy'], label='training accuracy')
-ax2.plot(history.history['val_accuracy'], label='validation accuracy')
-ax2.set_title('Accuracy curves')
-ax2.set_xlabel('Epoch')
-ax2.set_ylabel('Accuracy')
-ax2.legend()
+  # Plot the loss curves on the first subplot
+  ax1.plot(history.history['loss'], label='training loss')
+  ax1.plot(history.history['val_loss'], label='validation loss')
+  ax1.set_title('Loss curves')
+  ax1.set_xlabel('Epoch')
+  ax1.set_ylabel('Loss')
+  ax1.legend()
 
-# Adjust the spacing between subplots
-fig.tight_layout()
+  # Plot the accuracy curves on the second subplot
+  ax2.plot(history.history['accuracy'], label='training accuracy')
+  ax2.plot(history.history['val_accuracy'], label='validation accuracy')
+  ax2.set_title('Accuracy curves')
+  ax2.set_xlabel('Epoch')
+  ax2.set_ylabel('Accuracy')
+  ax2.legend()
 
-# Show the figure
-plt.show()
+  # Adjust the spacing between subplots
+  fig.tight_layout()
+
+  # Show the figure
+  plt.show()
+
+
+
+
 
 
 
@@ -165,25 +205,29 @@ from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
-# Generate predictions for all the test images
-predictions = model.predict(test_set)
+# Example usage:
+plot_confusion_matrix(model, test_set)
 
-# Extract labels from image file names
-labels = [filename.split('_')[0] for filename in test_set.filenames]
+def confusion_matrix(model, test_set):
+  # Generate predictions for all the test images
+  predictions = model.predict(test_set)
 
-# Initialize the label encoder
-label_encoder = LabelEncoder()
+  # Extract labels from image file names
+  labels = [filename.split('_')[0] for filename in test_set.filenames]
 
-# Fit and transform the labels to numerical values
-numeric_labels = label_encoder.fit_transform(labels)
+  # Initialize the label encoder
+  label_encoder = LabelEncoder()
 
-# First, let's transform all the prediction into the winners (otherwise each prediction gives us the 10 probabilities, but we only need the winner, the one our network thinks it is)
-pred = np.argmax(predictions, axis=1)
-# Now, compare the true labels of the test set, to our predicted winners
-cm = confusion_matrix(numeric_labels, pred)
+  # Fit and transform the labels to numerical values
+  numeric_labels = label_encoder.fit_transform(labels)
 
-# Make the confusion matrix a little more visually attractive
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+  # First, let's transform all the prediction into the winners (otherwise each prediction gives us the 10 probabilities, but we only need the winner, the one our network thinks it is)
+  pred = np.argmax(predictions, axis=1)
+  # Now, compare the true labels of the test set, to our predicted winners
+  cm = confusion_matrix(numeric_labels, pred)
 
-disp.plot(cmap=plt.cm.Blues)
-plt.show()
+  # Make the confusion matrix a little more visually attractive
+  disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+
+  disp.plot(cmap=plt.cm.Blues)
+  plt.show()
