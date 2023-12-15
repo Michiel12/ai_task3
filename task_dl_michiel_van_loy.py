5352a19e2380c6fa5cb0d8b4ac5e7bf094a6ae6f
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-12-15 15:51:15 +0100

    Update task_dl_michiel_van_loy.py

--- task_dl_michiel_van_loy.py
+++ task_dl_michiel_van_loy.py
@@ -30,7 +30,7 @@ for subdirectory in subdirectories:
     col1, col2, col3 = st.columns(3)
     
     # Show the category header only once
-    st.subheader(subdirectory)
+    st.header(subdirectory)
     
     # Show the first images of each category
     images_to_show = 3
@@ -40,12 +40,15 @@ for subdirectory in subdirectories:
         # Display the image in the respective column
         if i == 0:
             with col1:
+                st.subheader(f"Image: {i+1}")
                 st.image(img, width=200)
         elif i == 1:
             with col2:
+                st.subheader(f"Image: {i+1}")
                 st.image(img, width=200)
         elif i == 2:
             with col3:
+                st.subheader(f"Image: {i+1}")
                 st.image(img, width=200)
 
 # Create a bar chart
