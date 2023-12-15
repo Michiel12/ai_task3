2e7a2f6d66cf65b10003b29a82624650fb91e117
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-12-15 17:03:39 +0100

    Update task_dl_michiel_van_loy.py

--- task_dl_michiel_van_loy.py
+++ task_dl_michiel_van_loy.py
@@ -178,7 +178,7 @@ epochs = st.sidebar.slider("Select Number of Epochs", min_value=1, max_value=50,
 
 # Button to trigger training
 if st.sidebar.button('Train Model'):
-  training_set, validation_set = generate_augmented_data()
+  training_set, validation_set, test_set = generate_augmented_data()
   model = create_model()
   train_model(model, training_set, validation_set, epochs, steps_per_epoch)
 
